8614c7467a4d8b903357ef47e3981b78e5820df3
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-03-14 01:01:47 +0300

    fetch market tokens

--- contracts/KBMarket.sol
+++ contracts/KBMarket.sol
@@ -132,4 +132,23 @@ contract KBMarket is ReentrancyGuard {
     }
 
     //function the fetchMarketItems
+    //return the number of unsold items
+
+    function fetchMarketTokens() public view returns (MarketToken[] memory) {
+        uint itemCount = _tokenIds.current();
+        uint unsoldItemCount = _tokenIds.current() - _tokensSold.current();
+        uint currentIndex = 0;
+
+        //looping over the number of items created(if number has not been sold populate the array)
+        MarketToken[] memory items = new MarketToken[](unsoldItemCount); 
+        for(uint i = 0; i < itemCount; i++){
+            if(idToMarketToken[i + 1].owner == address(0)) {
+                uint currentId = i + 1;
+                MarketToken storage currentItem = idToMarketToken[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
 }
