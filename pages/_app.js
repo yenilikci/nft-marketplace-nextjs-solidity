723dafabace60cf0455d72faaa810de8dba10098
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-03-14 23:38:02 +0300

    Styling the DApp Ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import '../styles/globals.css'
 import Link from "next/link";
+import './app.css';
 
 function KBMarketplace({Component, pageProps}) {
     return (
         <div>
-            <nav className='border-b p-6' style={{backgroundColor: 'purple'}}>
-                <p className='text-4x1 font-bold text-white'>KB Marketplace</p>
+            <nav className='border-b p-6' style={{backgroundColor: 'darkblue'}}>
+                <h1 className='text-4x1 font-bold text-white'>KB Marketplace</h1>
                 <div className='flex mt-4 justify-center'>
                     <Link href='/'>
                         <a className='mr-4'>
