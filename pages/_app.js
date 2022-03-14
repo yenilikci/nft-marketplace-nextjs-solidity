499434b00410ffc028c2d3b06526a8da495e02f7
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-03-14 22:57:56 +0300

    DApp Application ui styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
 import '../styles/globals.css'
+import Link from "next/link";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function KBMarketplace({Component, pageProps}) {
+    return (
+        <div>
+            <nav className='border-b p-6' style={{backgroundColor: 'purple'}}>
+                <p className='text-4x1 font-bold text-white'>KB Marketplace</p>
+                <div className='flex mt-4 justify-center'>
+                    <Link href='/'>
+                        <a className='mr-4'>
+                            Main Marketplace
+                        </a>
+                    </Link>
+                    <Link href='/mint-item'>
+                        <a className='mr-4'>
+                            Mint Tokens
+                        </a>
+                    </Link>
+                    <Link href='/my-nfts'>
+                        <a className='mr-4'>
+                            My NFTs
+                        </a>
+                    </Link>
+                    <Link href='/account-dashboard'>
+                        <a className='mr-4'>
+                            Account Dashboard
+                        </a>
+                    </Link>
+                </div>
+            </nav>
+            <Component {...pageProps} />
+        </div>
+    )
 }
 
-export default MyApp
+export default KBMarketplace;
