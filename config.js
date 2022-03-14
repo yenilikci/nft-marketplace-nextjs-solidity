23ba4323f3c14a24a93d189223fb321e8498f5f0
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-03-14 23:58:22 +0300

    deploying smart contracts and running a local blockchain

---
+++ config.js
@@ -0,0 +1,2 @@
+export const nftmarketaddress = 0x8A791620dd6260079BF849Dc5567aDC3F2FdC318
+export const nftaddress = 0x610178dA211FEF7D417bC0e6FeD39F05609AD788
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
+import {ethers} from "ethers";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
+
 export default function Home() {
   return (
     <div>
