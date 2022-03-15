44a46ae48dc35909155a3d9b7fdfd7dd5ef534dd
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-03-15 22:43:17 +0300

    loading NFTs to the market function

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,53 @@
-import {ethers} from "ethers";
-import {useEffect, useState} from "react";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Web3Modal from "web3modal";
 
-export default function Home() {
-  return (
-    <div>
+import { nftaddresss, nftmarketaddress } from "../config";
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+import KBMarket from "../artifacts/contracts/KBMarket.sol/KBMarket.json";
 
-    </div>
-  )
+export default function Home() {
+  const [nfts, setNFTs] = useState([]);
+  const [loadingState, setLoadingState] = useState("not loaded");
+
+  useEffect(() => {
+    loadNFTs;
+  }, []);
+
+  async function loadNFTs() {
+    //what we want to load:
+    //provider, tokenContract, marketContract, data for our marketItems
+    const provider = new ethers.providers.JsonRpcProvider();
+    const tokenContract = new ethers.Contract(nftaddresss, NFT.abi, provider);
+    const marketContract = new ethers.Contract(
+      nftmarketaddress,
+      KBMarket.abi,
+      provider
+    );
+    const data = await marketContract.fetchMarketItems();
+
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await tokenContract.tokenURI(i.tokenId);
+        //we want get the token metadata - json
+        const meta = await axios.get(tokenUri);
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+          name: meta.data.name,
+          description: meta.data.description,
+        };
+        return item;
+      })
+    );
+    setNFTs(items);
+    setLoadingState("loaded");
+  }
+
+  return <div></div>;
 }
