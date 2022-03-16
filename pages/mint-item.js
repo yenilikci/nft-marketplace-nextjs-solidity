f4d34200e1b3d213c651b31f3f0181ca2e4a5a21
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-03-16 23:44:22 +0300

    minting nfts final functionality

--- pages/mint-item.js
+++ pages/mint-item.js
@@ -32,4 +32,51 @@ export default function MintItem() {
       console.log("Error uploading file:", error);
     }
   }
+
+  async function createMarket() {
+    const { name, description, price } = formInput;
+    if (!name || !description || !price || !fileUrl) return;
+    //upload to IPFS
+    const data = JSON.stringify({
+      name,
+      description,
+      image: fileUrl,
+    });
+    try {
+      const added = await client.add(data);
+      const url = `https://ipfs.infura.io:5001/api/v0/${added.path}`;
+      //run a function that creates sale and passes in the url
+      createSale(url);
+    } catch (error) {
+      console.log("Error uploading file:", error);
+    }
+  }
+
+  async function createSale(url) {
+    //create the items and list them on marketplace
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    //we want to create the token
+    let ethers = new ethers.Contract(nftaddress, NFT.abi, signer);
+    let transaction = await contract.mintToken(url);
+    let tx = await transaction.wait();
+    let event = tx.events[0];
+    let value = event.args[2];
+    let tokenId = value.toNumber();
+    const price = ethers.utils.parseUnits(formInput.price, "ether");
+
+    //list the item for sale on the marketplace
+    contract = new ethers.Contract(nftmarketaddress, KBMarket.abi, signer);
+    let listingPrice = await contract.getListingPrice();
+    listingPrice = listingPrice.toString();
+
+    transaction = await contract.makeMarketItem(nftaddress, tokenId, price, {
+      value: listingPrice,
+    });
+    await transaction.await();
+    router.push("/");
+  }
 }
