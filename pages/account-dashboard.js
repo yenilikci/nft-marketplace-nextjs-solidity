38333f9676dc3a8d7e971f56901d5142d449e1d7
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-03-17 23:52:51 +0300

    dapp code dashboard page

---
+++ pages/account-dashboard.js
@@ -0,0 +1,119 @@
+//we want to laod the users nfts and display
+import {ethers} from "ethers";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
+
+import {nftaddress, nftmarketaddress} from "../config";
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+import KBMarket from "../artifacts/contracts/KBMarket.sol/KBMarket.json";
+
+export default function MyAssets() {
+    const [nfts, setNFTs] = useState([]);
+    const [sold, setSold] = useState([]);
+    const [loadingState, setLoadingState] = useState("not loaded");
+
+    useEffect(() => {
+        loadNFTs();
+    }, []);
+
+    async function loadNFTs() {
+        //what we want to load:
+        //we want to get msg.sender hook up to the signer to display the owner nfts
+        const web3Modal = new Web3Modal();
+        const connection = await web3Modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+        const signer = provider.getSigner();
+
+        const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
+        const marketContract = new ethers.Contract(
+            nftmarketaddress,
+            KBMarket.abi,
+            provider
+        );
+        const data = await marketContract.fetchItemsCreated();
+
+        const items = await Promise.all(
+            data.map(async (i) => {
+                const tokenUri = await tokenContract.tokenURI(i.tokenId);
+                //we want get the token metadata - json
+                const meta = await axios.get(tokenUri);
+                let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+                let item = {
+                    price,
+                    tokenId: i.tokenId.toNumber(),
+                    seller: i.seller,
+                    owner: i.owner,
+                    image: meta.data.image,
+                    name: meta.data.name,
+                    description: meta.data.description,
+                };
+                return item;
+            })
+        );
+        //create a filtered array of items that have been sold
+        const soldItems = items.filter(i => i.sold);
+        setSold(soldItems);
+        setNFTs(items);
+        setLoadingState("loaded");
+    }
+
+    //function to buy nfts for market
+    async function buyNFT(nft) {
+        const web3Modal = new Web3Modal();
+        const connection = await web3Modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+            nftmarketaddress,
+            KBMarket.abi,
+            signer
+        );
+
+        const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
+        const transaction = await contract.createMarketSale(
+            nftaddress,
+            nft.tokenId,
+            {
+                value: price,
+            }
+        );
+
+        await transaction.wait();
+        loadNFTs();
+    }
+
+    if (loadingState === "loaded" && !nfts.length)
+        return <h1 className="px-20 py-7 text-4x1">You have not minted ant NFTs!</h1>;
+
+    return (
+        <div className="p-4 text-center">
+            <h1 className="text-2x1 py-2">Tokens Minted</h1>
+            <div className="px-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+                    {nfts.map((nft, i) => (
+                        <div key={i} className="border shadow rounded-x1 overflow-hidden">
+                            <img src={nft.image}/>
+                            <div className="p-4">
+                                <p
+                                    style={{height: "50px"}}
+                                    className="text-3x1 font-semibold"
+                                >
+                                    {nft.name}
+                                </p>
+                                <div style={{height: "60px", overflow: "hidden"}}>
+                                    <p className="text-gray-400">{nft.description}</p>
+                                </div>
+                            </div>
+                            <div className="p-4 bg-black">
+                                <p className="text-3x1 mb-4 font-bold text-white">
+                                    {nft.price} ETH
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
